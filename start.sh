4c5fc9747afca140b7b403ec6be2dc2e895c15d1
Soichi Hayashi <hayashis@iu.edu>
2017-04-11 12:33:01 -0400

    added code to figure out dwi resolution

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#make sure jq is installed on $SCA_SERVICE_DIR (used by status.sh to analyze progress)
-#if [ ! -f $SCA_SERVICE_DIR/jq ];
-#then
-#        echo "installing jq"
-#        wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O $SCA_SERVICE_DIR/jq
-#        chmod +x $SCA_SERVICE_DIR/jq
-#fi
-
 #mainly to debug locally
 if [ -z $SCA_WORKFLOW_DIR ]; then export SCA_WORKFLOW_DIR=`pwd`; fi
 if [ -z $SCA_TASK_DIR ]; then export SCA_TASK_DIR=`pwd`; fi
@@ -15,7 +7,6 @@ if [ -z $SCA_SERVICE_DIR ]; then export SCA_SERVICE_DIR=`pwd`; fi
 
 #clean up previous job (just in case)
 rm -f finished
-#jobid=`qsub -q preempt submit.pbs`
 jobid=`qsub $SCA_SERVICE_DIR/submit.pbs`
 echo $jobid > jobid
 
