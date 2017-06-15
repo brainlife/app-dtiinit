4b3c7473f66f3d44d4eb166cdd2c7b1a0370a1fc
kitchell <lindseykitchell21@gmail.com>
2017-06-15 12:52:00 -0400

    edited to run on VM env

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 #mainly to debug locally
 if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
-#if [ -z $ENV ]; then export ENV=IUHPC; fi
+if [ -z $ENV ]; then export ENV=IUHPC; fi
 
 #clean up previous job (just in case)
 rm -f finished
