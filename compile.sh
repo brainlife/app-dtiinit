25eb0e94cf9ced052118ca5e27732d9eba752039
Soichi Hayashi <hayashis@iu.edu>
2018-04-04 18:01:20 -0400

    included mrAnatomy/Segments (bet2) and templates directory on compiled binary

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
-mcc -m -R -nodisplay -d compiled dtiinit
+mcc -m -R -nodisplay -a /N/u/brlife/git/vistasoft/mrAnatomy/Segment -a /N/u/brlife/git/vistasoft/mrDiffusion/templates  -d compiled dtiinit
 exit
 END
 matlab -nodisplay -nosplash -r build
